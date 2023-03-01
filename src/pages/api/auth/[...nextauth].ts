662a572c08ca87f8ba95ef6c6b0f09ec274c2a95
riflan ahamed <riflan.ahamed@codimite.com>
2023-03-01 11:18:22 +0530

    fixed next-auth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,11 +1,12 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
-      clientId: process.env.NEXT_PUBLIC_GITHUB_ID || "",
-      clientSecret: process.env.NEXT_PUBLIC_GITHUB_SECRET || "",
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
     }),
     // ...add more providers here
   ],
