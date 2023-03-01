133f8dce6a7c5eda988c2e2797eb0910ff464535
riflan ahamed <riflan.ahamed@codimite.com>
2023-03-01 11:03:38 +0530

    added next-auth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,10 +4,24 @@ export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
-      clientId: process.env.NEXT_GITHUB_ID || "",
-      clientSecret: process.env.NEXT_GITHUB_SECRET || "",
+      clientId: process.env.NEXT_PUBLIC_GITHUB_ID || "",
+      clientSecret: process.env.NEXT_PUBLIC_GITHUB_SECRET || "",
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    async jwt({ token, account }: any) {
+      // Persist the OAuth access_token to the token right after signin
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token, user }: any) {
+      // Send properties to the client, like an access_token from a provider.
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
 };
 export default NextAuth(authOptions);
