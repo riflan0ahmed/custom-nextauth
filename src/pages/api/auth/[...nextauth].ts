8924d78d5f85afc08f3a889bdc608240079aa50a
riflan ahamed <riflan.ahamed@codimite.com>
2023-03-01 12:20:00 +0530

    added credenttial auth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,14 +1,50 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. "Sign in with...")
+      name: "Credentials",
+      // `credentials` is used to generate a form on the sign in page.
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        username: {
+          label: "Username",
+          type: "text",
+          value: "riflan0ahmed@gmail.com",
+        },
+        password: { label: "Password", type: "password", value: "12345678" },
+      },
+      async authorize(credentials: any, req) {
+        // Add logic here to look up the user from the credentials supplied
+        const user = {
+          id: "1",
+          name: "riflan",
+          email: "riflan0ahmed@gmail.com",
+          password: "12345678",
+        };
+
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user;
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null;
+
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      },
+    }),
+    // ...add more providers here
     GithubProvider({
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
     }),
-    // ...add more providers here
   ],
   callbacks: {
     async jwt({ token, account }: any) {
