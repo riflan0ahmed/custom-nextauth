2c5298f08a545c2d276277e573afa9476aba7223
riflan ahamed <riflan.ahamed@codimite.com>
2023-03-01 10:43:41 +0530

    intial commit

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,13 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: process.env.NEXT_GITHUB_ID || "",
+      clientSecret: process.env.NEXT_GITHUB_SECRET || "",
+    }),
+    // ...add more providers here
+  ],
+};
+export default NextAuth(authOptions);
